13324523b58f00923281d667347bdce1c507a07d
JunaidARahat <mjunaidrahat1990@gmail.com>
2024-08-30 16:28:34 +0500

    code update

--- my_bot.py
+++ my_bot.py
@@ -41,7 +41,7 @@ async def welcome(message: types.Message):
     Args:
         message (types.Message): _description_
     """
-    await message.reply("Hi\nI am a Chat Bot! Created by Bappy. How can i assist you?")
+    await message.reply("Hi\nI am a Chat Bot! Created by Junaid. How can i assist you?")
 
 
 
@@ -51,7 +51,7 @@ async def helper(message: types.Message):
     A handler to display the help menu.
     """
     help_command = """
-    Hi There, I'm a bot created by Bappy! Please follow these commands - 
+    Hi There, I'm a bot created by Junaid! Please follow these commands - 
     /start - to start the conversation
     /clear - to clear the past conversation and context.
     /help - to get this help menu.
